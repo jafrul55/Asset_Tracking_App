6c322c757c4807cf449fa9aecbed98672cbcc4a3
jafrul <jafrulalamtusher22@gmail.com>
2023-07-10 23:01:08 +0600

    first

--- asset_tracking/asset_management/admin.py
+++ asset_tracking/asset_management/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Company, Employee, Device, Asset, DeviceLog
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'company']
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['device_name', 'company']
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ['serial_number', 'company', 'assigned_date', 'return_date', 'condition']
+
+@admin.register(DeviceLog)
+class DeviceLogAdmin(admin.ModelAdmin):
+    list_display = ['asset', 'condition', 'log_date']
